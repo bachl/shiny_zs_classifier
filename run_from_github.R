9fa214fd854579934455b3667ab47ad7648d8c5a
Marko <marko.bachl@gmail.com>
2024-06-21 23:57:30 +0200

    Update run_from_github.R

--- run_from_github.R
+++ run_from_github.R
@@ -1,5 +1,4 @@
 if (!require("shiny")) install.packages("shiny")
 if (!require("httr2")) install.packages("httr2")
-if (!require("rio")) install.packages("rio")
 if (!require("tidyverse")) install.packages("tidyverse")
 shiny::runGitHub( "shiny_zs_classifier", "bachl")
