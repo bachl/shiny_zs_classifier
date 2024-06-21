5814e98cd0cd5d68672fa063ac63416148472dce
Marko <marko.bachl@gmail.com>
2024-06-21 23:56:11 +0200

    Update app.R

--- app.R
+++ app.R
@@ -10,7 +10,7 @@ ui <- fluidPage(
       numericInput("max_tokens", "Maximum of tokens:", value = 80L),
       textAreaInput("prompt", "Prompt:", rows = 8,
                     value = 'I will show you a social media comment. Decide whether it is sexist or not sexist. This task defines sexism as: "Any abuse or negative sentiment that is directed towards women based on their gender, or on the combination of their gender with one or more other identity attributes (e.g. Black women, Muslim women, Trans women)."\n\nYou should assign the comment a numeric label, 1 or 0.\n1. The comment is sexist.\n0. The comment is not sexist.\n\nAnswer in JSON format with the template below.\n\n{\n  \"label\": 1,\n  \"motivation\": \"Briefly explain why you chose this label.\"\n}\n'),
-      fileInput("file", "Choose CSV File",
+      fileInput("file", "Choose CSV File with ';' as separator:",
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".csv")),
@@ -27,7 +27,8 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   results <- eventReactive(input$submit, {
-    txts = rio::import(input$file$datapath)[[input$column]]
+    # txts = rio::import(input$file$datapath)[[input$column]]
+    txts = read.csv2(input$file$datapath)[[input$column]]
     classify(api_key = input$api_key,
              text_file = txts,
              prompt_file = input$prompt,
